ac4316a16289870478549f4507daa3d7de2f0712
dodoflyaway <43408476+dodoflyaway@users.noreply.github.com>
2021-01-03 02:14:53 +0400

    Add files via upload

---
+++ div_by_10.c
@@ -0,0 +1,32 @@
+// peice of code used to find the length of a number 
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int x,j;
+	printf("enter a number to find the lenght of \n");
+	scanf("%d",&x);
+	int len = 0;// to store the leght of the number
+	int loop_flag =  0; //flag to stop the exuction of the loop 
+	while(loop_flag!=1)
+	{
+		
+		x = x/10;  //  30 = 32/10
+		printf("%d\n",x);
+	    if(x==0)
+	    {
+	    	
+	    	loop_flag = 1;
+	    	
+		}
+		
+		len++;
+		
+		
+	} 
+	
+    printf("len is %d\n",len);
+    return 0;
+	
+	
+}
